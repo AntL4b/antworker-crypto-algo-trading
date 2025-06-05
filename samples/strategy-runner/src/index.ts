5091eae7c3c0d2bd6520e1f2ae24a5faff345d3a
AntoineLep <6230724+AntoineLep@users.noreply.github.com>
2025-06-05 21:39:24 +0200

    reorganise strategy runner sample index file

--- samples/strategy-runner/src/index.ts
+++ samples/strategy-runner/src/index.ts
@@ -1,17 +1,18 @@
 import { antworker, consoleLoggerHandler, fileLoggerHandler, Logger, LogLevelEnum } from "@antl4b/antworker-crypto-algo-trading";
 import { MyStrat } from "./my-strat";
 
-
-const app = antworker("sample-strategy-runner");
+const APP_NAME = "sample-strategy-runner";
 
 // Configure logger
 const logger = Logger.getInstance();
 logger.setLogLevel(LogLevelEnum.Trace);
 logger.addHandler(consoleLoggerHandler());
 logger.addHandler(fileLoggerHandler({
-	filePrefix: "sample-strategy-runner",
+	filePrefix: APP_NAME,
 	logDir: "logs",
 }));
 
+// Configure app
+const app = antworker(APP_NAME);
 app.setStrategy(new MyStrat());
 app.runStrategy();
